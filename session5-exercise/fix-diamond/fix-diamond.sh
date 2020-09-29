074b20a5d123523b55fb0642d13e0df50493e8b8
Nikolaus Sonnenschein <niso@dtu.dk>
2020-09-29 06:50:33 +0000

    Add diamond fix

---
+++ session5-exercise/fix-diamond/fix-diamond.sh
@@ -0,0 +1,4 @@
+wget http://github.com/bbuchfink/diamond/releases/download/v0.9.14/diamond-linux64.tar.gz
+tar xzf diamond-linux64.tar.gz
+cp diamond /home/jovyan/.local/bin/
+cp diamond /opt/conda/bin
